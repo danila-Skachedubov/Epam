5899721d8b05a9f3993a59a86b2be3714e04faaf
drDanik <SKADA2012@yandex.ru>
2021-12-29 23:22:38 +0300

    Adding a menu
    1.Added a description of the program
    2. Added menu functionality

---
+++ Task 1/Task1.1/Task1.1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Task1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, user!" + "\n" + "This is homework 1.1. To select the desired task, enter the corresponding number." + "\n\n");
+            Console.WriteLine("1 - for RECTANGLE(1.1.1)\n");
+            Console.WriteLine("2 - for TRIANGLE(1.1.1)\n");
+            Console.WriteLine("3 - for ANOTHER TRIANGLE(1.1.1)\n");
+            Console.WriteLine("4 - for X-MAS TREE(1.1.1)\n");
+            Console.WriteLine("5 - for SUM OF NUMBERS(1.1.1)\n");
+            Console.WriteLine("6 - for FONT ADJUSTMENT(1.1.1)\n");
+            Console.WriteLine("7- for ARRAY PROCESSING(1.1.1)\n");
+            Console.WriteLine("8- for NO POSITIVE(1.1.1)\n");
+            Console.WriteLine("9- for NON-NEGATIVE SUM(1.1.1)\n");
+            Console.WriteLine("10- for 2D ARRAY(1.1.1)\n");
+            Console.WriteLine("0- for exit\n");
+          
+            int number_selection = int.Parse(Console.ReadLine());
+            switch (number_selection)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+
+                    break;
+                case 0:
+
+                    break;
+                default:
+                    Console.WriteLine("enter correct value");
+                    break;
+
+            }
+            static int Task1()
+            {
+
+            }
+
+        }
+    }
+}
