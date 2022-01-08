dcd255c19c75dda16c1adf4b22a128b9e9222109
drDanik <SKADA2012@yandex.ru>
2022-01-08 19:33:11 +0300

    Task 1.2.2
    Task solution Doubler

---
+++ Task 1/Task1.2.2/Task1.2.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Task1._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую строку: ");
+            string  str1 = Console.ReadLine();
+            Console.Write($"{ Environment.NewLine }Введите вторую строку: ");
+            string str2 = Console.ReadLine();
+            str2.ToCharArray();
+            string str3 = " ";
+            foreach (char i in str1)
+                if (!str2.Contains(i))
+                    str3 += i;
+                else
+                {
+               
+                    str3 += i;
+                    str3 += i;
+                }
+            Console.WriteLine($"Результат: { str3 }");
+        }
+    }
+}
