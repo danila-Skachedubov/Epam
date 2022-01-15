510da4cb6491539c00b9e743760ef4ec6f625b99
drDanik <SKADA2012@yandex.ru>
2022-01-15 22:55:51 +0300

    Implemented task 2.1.1
    Completed the items specified for the assignment

---
+++ Task 2/Task 2.1.1/Task 2.1.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Task_2._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arr = "Privet";
+            string arr2 = "Hello";
+            char c = 'v';
+            string arr3 = string.Empty;
+           
+            My_String str = new My_String(arr);
+            My_String str2 = new My_String(arr2);
+            My_String str3 = new My_String(arr3);
+
+            Console.WriteLine("Сравнение строки arr & arr2");
+            str.Comparison(str2);//реализация cравнения строк
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine("Конкатинация строк arr & arr2");
+            str3.Concatenation(str, str2);//реазизация конкатинации
+            Console.WriteLine(str3.Symbols);
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine("Поиск символа 'v' в строке arr" );
+            str.Search(c);//реализация поиска символа
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine("Конвертацаия строки");
+            str.ConvertString();
+            str.ConvertChar();
+            Console.WriteLine(str.Symbols.GetType());//нреализация приведения из/к
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine("Удаление символа 'v' в строке  arr");
+            str.DelitSymb(c);//реализация удаления символа 
+            Console.WriteLine(str.Symbols);
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine("Инверсия строки arr");
+            str.Inversion();//реализация инверсии строки
+            Console.Write(Environment.NewLine);
+
+            Console.WriteLine("0-ой объект в индексаторе");
+            My_String[] strings;
+            var text = new[] { new My_String("mama"), new My_String("papa"), new My_String("mama"), new My_String("mama") };
+            My_String firstword = text[0];
+            Console.WriteLine(firstword.Symbols);//реализация индексатора
+        }
+    }
+}
---
+++ Task 2/Task 2.1.1/Task 2.1.1/Stringg.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._1._1
+{
+    public class My_String
+    {
+        //поля
+        private char[] _symbols = null;
+        public int length = 0;
+
+        public char[] Symbols { get => _symbols; set => _symbols = value; }
+
+        public My_String()
+        {
+            Symbols = new char[100];
+        }
+        public My_String(string array)
+        {
+           
+            this.Symbols = array.ToCharArray();
+        }
+       
+        
+        public  bool Comparison (My_String a)
+        {
+            if(Symbols.Length == a.Symbols.Length)
+            {
+                for (int i = 0; i < Symbols.Length; i++)
+                {
+                    for (int j = 0; j < length; j++)
+                    {
+                        if(Symbols[i] != a.Symbols[i])
+                        Console.WriteLine("False");
+                        return false;
+                    }
+                }
+                Console.WriteLine("True");
+                return true;
+            }
+            else
+            Console.WriteLine("False");
+            return false;
+        }
+        public char[] Concatenation(My_String a, My_String b)
+        {
+            var sum = a.Symbols.Concat(b.Symbols).ToArray();
+            return Symbols = sum;
+        }
+        public bool Search(char c)
+        {
+            for (int i = 0; i < Symbols.Length; i++)
+            {
+                if(Symbols[i] == c)
+                {
+                    Console.WriteLine("True");
+                    return true;
+                }
+            }
+            Console.WriteLine("False");
+            return false;
+           
+        }
+        public string ConvertString()
+        {
+            return new string(Symbols);
+        }
+        public char[] ConvertChar()
+        {
+            
+           char[] toReturn = new char[Symbols.Length];
+
+           for (int i = 0; i < Symbols.Length; i++)
+           {
+                    toReturn[i] = Symbols[i];
+           }
+           return toReturn;           
+        }
+        public char[] DelitSymb(char c)
+        {
+            for (int i = 0; i < Symbols.Length; i++)
+            {
+                if(Symbols[i] == c)
+                {
+                    Symbols[i] = '\0';
+                }  
+            }
+            return Symbols;
+        }
+        
+        public char[] Inversion()
+        {
+            for (int i = 0; i < Symbols.Length/2; i++)
+            {                char c = '\0';
+                c = Symbols[i];
+                Symbols[i] = Symbols[Symbols.Length - i-1];
+                Symbols[Symbols.Length - i-1] = c;
+            }
+            Console.WriteLine(Symbols);
+            return Symbols;
+        }
+        My_String[] strings;
+        public My_String this[int index]
+        {
+            get => strings[index];
+            set => strings[index] = value;
+        }
+    }  
+}
