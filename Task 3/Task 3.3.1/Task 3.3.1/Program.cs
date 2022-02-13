26eaea86e7049877f81b5f7788b10f0116ee7793
drDanik <SKADA2012@yandex.ru>
2022-02-13 13:07:58 +0300

    Task 3.3.1 done
    Task 3.3.1 is done

---
+++ Task 3/Task 3.3.1/Task 3.3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_3._3._1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            double[] array = { 1, 2, 3, 4, 5 };
+
+            array.Transformation(x => x *2);
+            
+            double Sum = array.Summ();
+            Console.WriteLine(Sum);
+            double Average = array.Average();
+            Console.WriteLine(Average);
+            double Elementscommon = array.MostCommonElement();
+            Console.WriteLine(Elementscommon);
+        }
+    }
+}
+
